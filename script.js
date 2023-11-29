5f5890b57b5fa1f13f85d3ee70a408732759bac0
Arithz <harithzfathi2001@gmail.com>
2023-11-30 02:46:25 +0800

    add more frontend design

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.getElementById("submit");
   const convertType = document.getElementsByName("converttype");
+  const fileInput = document.getElementById("dropzone-file");
 
   submitButton.addEventListener("click", startConvert);
   convertType.forEach((radio) => {
     radio.addEventListener("change", resetFileInput);
   });
+
+  fileInput.addEventListener("change", () => {
+    for (let i = 0; i < fileInput.files.length; i++) {
+      addFileNamesToDOM(fileInput.files[i].name);
+    }
+  });
 });
+
+function addFileNamesToDOM(filename) {
+  const container = document.createElement("div");
+  container.classList.add("uploads");
+  const names = document.createElement("p");
+  names.innerHTML = filename;
+  names.setAttribute("data-filename", filename);
+
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("transition");
+  removeBtn.innerHTML = "x";
+
+  removeBtn.addEventListener("click", () => {
+    removeFile(filename);
+  });
+
+  container.appendChild(names);
+  container.appendChild(removeBtn);
+  filenames.appendChild(container);
+}
+
+function removeFile(filename) {
+  const fileinput = document.getElementById("dropzone-file");
+  const filteredFiles = [...fileinput.files].filter((file) => file.name !== filename);
+  const dataTransfer = new DataTransfer();
+  filteredFiles.forEach((file) => dataTransfer.items.add(file));
+  fileinput.files = dataTransfer.files;
+  const container = document.querySelector(`[data-filename="${filename}"]`).parentElement;
+  container.remove();
+}
 
 function startConvert(e) {
   e.preventDefault(); // Prevent default form submission
 
   let convertmode = document.querySelector('input[name="converttype"]:checked').value;
-  let fileInput = document.getElementById("file");
+  let fileInput = document.getElementById("dropzone-file");
   let file = fileInput.files;
 
   // check if file is selected
@@ -24,13 +61,17 @@ function startConvert(e) {
   // check if one of the file is not the same format as the other
   const fileFormats = [...file].map((file) => file.name.split(".").pop());
   if (fileFormats.some((format) => format !== fileFormats[0])) {
-    if(convertmode === 'pdf' ) alert("Please select only pdf files");
+    if (convertmode === "pdf") alert("Please select only pdf files");
     else alert("Please select only txt files");
     return;
-  }else{
-    if(convertmode === 'pdf' && fileFormats[0] !== 'pdf') return alert("Please select only pdf files");
-    if(convertmode === 'txt' && fileFormats[0] !== 'txt') return alert("Please select only txt files");
+  } else {
+    if (convertmode === "pdf" && fileFormats[0] !== "pdf")
+      return alert("Please select only pdf files");
+    if (convertmode === "txt" && fileFormats[0] !== "txt")
+      return alert("Please select only txt files");
   }
+
+  disableUpload();
 
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "/converter.php", true);
@@ -44,6 +85,7 @@ function startConvert(e) {
       }
 
       logReponse(xhr);
+      enableUpload();
     }
   };
 
@@ -55,6 +97,25 @@ function startConvert(e) {
   formData.append("query", convertmode);
 
   xhr.send(formData);
+}
+
+function disableUpload() {
+  const box = document.getElementById("box");
+  box.classList.add("disabled");
+  const toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = `<svg aria-hidden="true" id = "loadingsvg" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg><p class="text-cs-black2 text-sm md:text-base font-bold">Converting...</p>`;
+  document.body.appendChild(toast);
+}
+
+function enableUpload() {
+  const box = document.getElementById("box");
+  box.classList.remove("disabled");
+  const toast = document.querySelector(".toast");
+  toast.remove();
 }
 
 function logReponse(xhr) {
@@ -83,11 +144,12 @@ function logReponse(xhr) {
 
   // alert user that conversion is done
   console.log(files);
-  alert("done");
+  // alert("done");
 }
 
 function resetFileInput() {
   const fileInput = document.getElementById("file");
+  if (!fileInput) return;
   fileInput.value = "";
   fileInput.files = null;
 }
